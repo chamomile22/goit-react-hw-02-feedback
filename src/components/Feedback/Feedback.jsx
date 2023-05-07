e88f9bf2cd14620f1f2bd5f6e2c177ede2744523
Yana Babak <yana.i.babak@gmail.com>
2023-05-07 19:08:40 +0300

    step 3 done

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Section } from 'components/Section/Section';
+import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+import { Statistics } from 'components/Statistics/Statistics';
 
 export class Feedback extends React.Component {
   state = {
@@ -7,19 +10,9 @@ export class Feedback extends React.Component {
     bad: 0,
   };
 
-  handleGoodFeedback = () => {
+  handleLeaveFeedback = option => {
     this.setState(prevState => ({
-      good: (prevState.good += 1),
-    }));
-  };
-  handleNeutralFeedback = () => {
-    this.setState(prevState => ({
-      neutral: (prevState.neutral += 1),
-    }));
-  };
-  handleBadFeedback = () => {
-    this.setState(prevState => ({
-      bad: (prevState.bad += 1),
+      [option]: prevState[option] + 1,
     }));
   };
 
@@ -28,48 +21,36 @@ export class Feedback extends React.Component {
     total = good + neutral + bad;
     return total;
   };
+
   countPositiveFeedbackPercentage = (good, neutral, bad) => {
-    let percentage = 0;
-    console.log(percentage);
-    percentage = (good / (good + neutral + bad)) * 100;
-    console.log(percentage);
-    return percentage;
+    let positivePercentage = 0;
+    positivePercentage = (good / (good + neutral + bad)) * 100;
+    return positivePercentage;
   };
 
   render() {
     const { good, neutral, bad } = this.state;
     return (
       <>
-        <h2>Please leave your feedback</h2>
-        <ul>
-          <li>
-            <button type="button" onClick={this.handleGoodFeedback}>
-              Good
-            </button>
-          </li>
-          <li>
-            <button type="button" onClick={this.handleNeutralFeedback}>
-              Neutral
-            </button>
-          </li>
-          <li>
-            <button type="button" onClick={this.handleBadFeedback}>
-              Bad
-            </button>
-          </li>
-        </ul>
-        <h2>Statistics</h2>
-        <p>Good:{good}</p>
-        <p>Neutral:{neutral}</p>
-        <p>Bad:{bad}</p>
-        <p>Total:{this.countTotalFeedback(good, neutral, bad)}</p>
-        <p>
-          Percentage:
-          {console.log(
-            this.countPositiveFeedbackPercentage(good, neutral, bad)
-          )}
-          %
-        </p>
+        <Section title="Please leave your feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.handleLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback(good, neutral, bad)}
+            positivePercentage={this.countPositiveFeedbackPercentage(
+              good,
+              neutral,
+              bad
+            )}
+          />
+        </Section>
       </>
     );
   }
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <>
+      <ul>
+        {options.map(option => {
+          return (
+            <li key={option}>
+              <button type="button" onClick={() => onLeaveFeedback(option)}>
+                {option}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
